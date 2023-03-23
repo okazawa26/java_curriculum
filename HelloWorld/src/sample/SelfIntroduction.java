8daf732571122efe2e61c2d87ab2920d19f90177
okazawa26 <okazawa.ztmy@gmail.com>
2023-03-23 22:54:09 +0900

    1.3 Java導入

---
+++ HelloWorld/src/sample/SelfIntroduction.java
@@ -0,0 +1,24 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// 氏名
+		String name = "岡澤 亮太";
+		// 年齢
+		int age = 21;
+		// 趣味
+		String hobby = "映画鑑賞";
+
+
+		/*
+		* 自己紹介文を出力する処理
+		*/
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+
+	}
+
+}
