a620c8c5234adb20717eb677add394ee4d72f446
okazawa <okazawa.ztmy@gmail.com>
2023-06-09 22:52:00 +0900

    Qes5 1~20の掛け算 修正

--- java_curriculum/src/curriculum_B/Qes5.java
+++ java_curriculum/src/curriculum_B/Qes5.java
@@ -3,22 +3,33 @@ package curriculum_B;
 public class Qes5 {
 	public static void main(String[] args) {
 
-		// 外側のループ 1～9まで回す
+		// I * J = Z の式
+		// 外側のループI 1～9まで回す
 		for (int i =1; i <= 9; i++) {
 
-			// 内側のループ 1～20まで回す
+			// 内側のループJ 1～20まで回す
 			for (int j = 1; j <= 20; j++) {
 
-				// iとjの掛け算の結果を変数に代入
+
+				// IとJの掛け算の結果を変数に代入
 
 				int result = i * j;
-				
 
-				// 結果が3桁以上ならそのまま出力し、1桁しかない場合は0をつけて3桁で出力
-				 // 内側のループの整数が20以外の時 || を出力し、整数が20であれば改行する
-				System.out.printf("%03d * %03d = %03d%s", j , i , result, (j != 20 ? " || " : "\n"));
-				
-				
+
+				// 内側のループが20以外のとき、「  ||  」を出力
+				if (j < 20) {
+
+					// 三桁で出力
+					System.out.printf("%03d * %03d = %03d  ||  ", j, i, result);
+
+					//  内側のループが20のとき改行
+				} else {
+
+					// 三桁で出力
+					System.out.printf("%03d * %03d = %03d\n", j, i, result);
+
+
+				}
 			}
 		}
 	}
